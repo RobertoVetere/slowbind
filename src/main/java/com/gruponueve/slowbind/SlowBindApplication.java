daccaa5afa2e23cab3839c60eefc1ff9f2286ffe
Roberto Vetere <Roberto.vetere@gmail.com>
2023-11-16 12:08:00 +0100

    first commit

---
+++ src/main/java/com/gruponueve/slowbind/SlowBindApplication.java
@@ -0,0 +1,13 @@
+package com.gruponueve.slowbind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlowBindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlowBindApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gruponueve/slowbind/SlowBindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gruponueve.slowbind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlowBindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
