d4054161551c4c16997f682342cfea61ea636de7
Juan Milla <tokyumilla@gmail.com>
2023-11-16 15:44:32 +0100

    dtos

---
+++ src/main/java/com/gruponueve/slowbind/dtos/InterestPointResponseDTO.java
@@ -0,0 +1,14 @@
+package com.gruponueve.slowbind.dtos;
+
+import com.gruponueve.slowbind.enums.Zona;
+
+
+public class InterestPointResponseDTO {
+    private String name;
+    private String descripcion;
+    private Double rating;
+    private Zona zona;
+    private double longitud;
+    private double latitud;
+    private String imageReference;
+}
