d757d6ae6572723c4f0cc9b2c03a12e4879d8704
Roberto Vetere <Roberto.vetere@gmail.com>
2023-11-16 23:48:05 +0100

    FIX routing json

--- src/main/java/com/gruponueve/slowbind/repository/IInterestPointRepository.java
+++ src/main/java/com/gruponueve/slowbind/repository/IInterestPointRepository.java
@@ -15,29 +15,29 @@ public interface IInterestPointRepository extends JpaRepository<InterestPoint, I
     @Query("select i from InterestPoint i where i.name like concat('%', ?1, '%')")
     List<InterestPoint> findByNameContains(String name);
 
-    @Query("select i from InterestPoint i where i.categoria = ?1")
+    @Query("select i from InterestPoint i where i.category = ?1")
     List<InterestPoint> findByCategoria(Categoria categoria);
 
     @Query("select i from InterestPoint i where i.rating >= ?1")
     List<InterestPoint> findByRatingGreaterThanEqual(double rating);
 
-    @Query("select i from InterestPoint i where i.categoria = ?1 and i.zona = ?2")
+    @Query("select i from InterestPoint i where i.category = ?1 and i.zone = ?2")
     List<InterestPoint> findByCategoriaAndZona(Categoria categoria, Zona zona);
 
-    @Query("select i from InterestPoint i where i.rating >= ?1 and i.zona = ?2")
+    @Query("select i from InterestPoint i where i.rating >= ?1 and i.zone = ?2")
     List<InterestPoint> findByRatingGreaterThanEqualAndZona(double rating, Zona zona);
 
-    @Query("select i from InterestPoint i where i.rating >= ?1 and i.categoria = ?2")
+    @Query("select i from InterestPoint i where i.rating >= ?1 and i.category = ?2")
     List<InterestPoint> findByRatingGreaterThanEqualAndCategoria(double rating, Categoria categoria);
 
 
 
-    @Query("select i from InterestPoint i where i.rating >= ?1 and i.categoria = ?2 and i.zona = ?3")
+    @Query("select i from InterestPoint i where i.rating >= ?1 and i.category = ?2 and i.zone = ?3")
     List<InterestPoint> findByRatingGreaterThanEqualAndCategoriaAndZona(double rating, Categoria categoria, Zona zona);
 
 
 
-    @Query("select i from InterestPoint i where i.zona = ?1")
+    @Query("select i from InterestPoint i where i.zone = ?1")
     List<InterestPoint> findByZona(Zona zona);
 
 }
